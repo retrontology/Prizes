b4647a8c00426acddb833a86aebe4c3026c78b5f
retrontology <retrontology@hotmail.com>
2016-03-03 21:34:04 -0600

    Bare Bones

---
+++ src/com/retrontology/prizes/Prizes.java
@@ -0,0 +1,26 @@
+package com.retrontology.prizes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Prizes extends JavaPlugin {
+	
+	/* Class Variables */
+	
+
+	/* Startup */
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	
+	/* Shutdown */
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	
+	/* Class Methods */
+	
+}
